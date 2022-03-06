cb71dabfbfaa9b33aef434c3bb9438d2c3608618
BorisKarakolev <boriskarakolev@hotmail.com>
2022-03-06 21:19:59 +0100

    Dev vs Prod env. Moved to the Client side

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Emaily</h2>
+      <h3>
+        <a href="/auth/google">Sign In With Google</a>
+      </h3>
+    </div>
+  );
+};
+
+export default App;
