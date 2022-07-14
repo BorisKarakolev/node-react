5525bce711fc6d2ea450e043c2688cca6c038582
Boris Karakolev <boriskarakolev@hotmail.com>
2022-07-14 22:19:12 +0200

    Add surveys in the backend add mailer

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const keys = require("./config/keys");
+
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 const app = express();
@@ -23,6 +25,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
